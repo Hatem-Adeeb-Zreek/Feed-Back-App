1fd3300d7726f571a3b99738c358c018f08d6e51
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2022-11-02 16:08:18 +0100

    dynamic values and lists with mapping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
+  const title = 'Feed Back App';
+  const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  const comments = [
+    {id:1, text: 'comment1'},
+    {id:2, text: 'comment2'},
+    {id:3, text: 'comment3'}
+  ];
   return (
     <div className="container">
-       <h1>Feed Back App</h1>
+       <h1>{title}</h1>
+       <p>{text}</p>
+       <h3>Comments: {comments.length}</h3>
+       <ul>
+         {comments.map((comment, index)=> (
+           <li key={index}>{comment.text}</li>
+         ))}
+       </ul>
     </div>
   )
 }
