ade9640b637c0995e18e98e26025c4781bd2bd34
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2022-11-02 16:17:57 +0100

    ternary operator for conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ function App() {
     {id:2, text: 'comment2'},
     {id:3, text: 'comment3'}
   ];
+  const showComments = true; // false to hide
+  const commentBlock = (
+    <div className="comments">
+    <h3>Comments: {comments.length}</h3>
+    <ul>
+      {comments.map((comment, index)=> (
+        <li key={index}>{comment.text}</li>
+      ))}
+    </ul>
+   </div>
+  );
   return (
     <div className="container">
        <h1>{title}</h1>
        <p>{text}</p>
-       <h3>Comments: {comments.length}</h3>
-       <ul>
-         {comments.map((comment, index)=> (
-           <li key={index}>{comment.text}</li>
-         ))}
-       </ul>
+       {showComments && commentBlock}
+      
     </div>
   )
 }
